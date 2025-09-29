b0d5f3ef407b1e3837cfbdd7799fa4e015dd0272
Jakob <45669207+gue-ni@users.noreply.github.com>
2025-09-29 10:49:46 +0200

    Create for-each.lisp

---
+++ tests/programs/for-each.lisp
@@ -0,0 +1,13 @@
+(defmacro for-each-1 (lst)
+  (if (null? lst)
+    nil
+   `(progn
+      (defun my-loop (l)
+         (if (null? l)
+            nil
+            (progn
+               (let ((el (car l))) (println el))
+               (my-loop (cdr l)))))
+      (my-loop ,lst))))
+
+(for-each-1 (list 1 2 3 4 5))
