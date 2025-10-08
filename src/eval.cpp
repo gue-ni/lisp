b92678de0e5fa53b04d3a314bc198bff233dae2f
Jakob <45669207+gue-ni@users.noreply.github.com>
2025-10-08 07:42:27 +0200

    Update eval.cpp

--- src/eval.cpp
+++ src/eval.cpp
@@ -147,6 +147,7 @@ std::string init_script()
 
 void eval_profile( Context & context, const IO & io )
 {
+
   const char * home = getenv( "HOME" );
   assert( home != nullptr );
 
@@ -164,6 +165,27 @@ void eval_profile( Context & context, const IO & io )
   std::string program = ss.str();
 
   ( void ) eval( program, context, io );
+
+  const std::string lib = R"(
+; test if a symbol is defined
+(defmacro symbolp (s)
+  `(let ((e ,s)) (not (error? e))))
+
+; if symbol is defined, return the symbol value, otherwise return the symbol name as a string
+(defmacro sh-arg (x)
+  `(if (symbolp ,x) ,x (symbol-name x)))
+
+; execute shell command, no quotes needed
+; usage:
+; (sh ls -la)
+; (let ((my-file "my_file.txt")) (sh touch my-file))
+(defmacro sh (&rest args)
+  `(apply exec (map sh-arg args)))
+
+
+)";
+
+  (void) eval(lib, context, io);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
